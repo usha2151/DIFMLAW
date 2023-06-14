687a6fb633ad82af1c2b9de650b23d6e131d4bad
bobyswaroop <bobyswaroop555024@gmail.com>
2023-06-14 09:48:04 +0530

    job page

---
+++ src/components/job_description_page/Lawyers_profile_card/Lawyers_profile_card.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Lawyers_profile_card = () => {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-lg-8'>
+              <div className='row'>
+                <div className='col-lg-6'>
+                    <div className='row'>
+                        <div className='col-lg-6'>
+
+                        </div>
+                        <div className='col-lg-6'>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className='col-lg-6'></div>
+              </div>
+            </div>
+            <div className='col-lg-4'>
+
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Lawyers_profile_card
