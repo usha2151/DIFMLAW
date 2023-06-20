3b20a4bfe490f5cdee86a7e6da5387fc22907bc7
bobyswaroop <bobyswaroop555024@gmail.com>
2023-06-20 14:30:30 +0530

    category count

--- src/components/Hero/CategoryCards.jsx
+++ src/components/Hero/CategoryCards.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { query, collection, getDocs, where, getCountFromServer } from "firebase/firestore";
+import { db } from '../../firebase';
 const CategoryCards = ({logo, type,  avalilabe}) => {
+  const [Count, setCount] = useState(0);
+  const count_cat = async () =>{
+    const q = query(collection(db, "lawyers"), where("specialization", "==", type))
+
+    const snapshot = await getCountFromServer(q);
+    setCount(snapshot.data().count);
+  
+
+  }
+  useEffect(()=>{
+    count_cat();
+  },[])
   return (
     <>
       <div className='col-lg-3 p-4'>
@@ -9,7 +22,7 @@ const CategoryCards = ({logo, type,  avalilabe}) => {
               <img src={logo} style={{marginLeft:"30%"}} />
           <p className='mt-2 mx-2' >{type}</p>
           <div className='d-flex'>
-          <h6 className='mt-2'>{ avalilabe}</h6>
+          <h6 className='mt-2'>{Count}{ avalilabe}</h6>
           <div><i class="bi bi-arrow-right ms-2 fs-4"></i></div>
           </div>
           
